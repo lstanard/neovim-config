65ae878d3fbb1f97420a65867e68e0fd87f34f7d
Charlie Stanard <charlie.stanard@guildeducation.com>
2023-03-15 17:55:59 -0500

    Fix whitespace

--- barebones/init.lua
+++ barebones/init.lua
@@ -27,7 +27,7 @@ vim.opt.rtp:prepend(lazypath)
 -- Specify plugins
 -----------------------------------------------------------
 
-require("lazy").setup({
+require('lazy').setup({
   -- colorschemes
   {
     'catppuccin/nvim',
@@ -44,47 +44,47 @@ vim.cmd.colorscheme('catppuccin-frappe')
 -----------------------------------------------------------
 
 -- General
-vim.opt.autoread = true			-- automatically re-read file if a change was detected outside of vim
--- vim.opt.cc = '90'			-- set an 90 column border
-vim.opt.clipboard = 'unnamedplus'	-- sync clipboard between OS and neovim
-vim.opt.completeopt = 'menuone,noinsert,noselect'		-- set completeopt to have a better completion experience
-vim.opt.cursorline = true		-- highlight current cursorline
-vim.opt.errorbells = false		-- disable bell sound for error messages
--- vim.opt.mouse = 'v'			-- enable mouse (in visual mode)
-vim.opt.hidden = true			-- TODO: understand this better
-vim.opt.number = true			-- always show line numbers
-vim.opt.relativenumber = true		-- use relative line numbers
-vim.opt.showmatch = true		-- highlight matching brackets 
-vim.opt.showmode = true			-- show vim mode in status line (default true)
-vim.opt.signcolumn = 'yes'		-- always show the sign column
-vim.opt.splitbelow = true		-- set preview window to appear at bottom
-vim.opt.splitright = true   		-- set preview window to appear on right
-vim.opt.syntax = 'on'			-- enable syntax highlighting
-vim.opt.termguicolors = true		-- colorscheme support
+vim.opt.autoread = true         -- automatically re-read file if a change was detected outside of vim
+-- vim.opt.cc = '90'            -- set an 90 column border
+vim.opt.clipboard = 'unnamedplus'                   -- sync clipboard between OS and neovim
+vim.opt.completeopt = 'menuone,noinsert,noselect'   -- set completeopt to have a better completion experience
+vim.opt.cursorline = true       -- highlight current cursorline
+vim.opt.errorbells = false      -- disable bell sound for error messages
+-- vim.opt.mouse = 'v'          -- enable mouse (in visual mode)
+vim.opt.hidden = true           -- TODO: understand this better
+vim.opt.number = true           -- always show line numbers
+vim.opt.relativenumber = true   -- use relative line numbers
+vim.opt.showmatch = true        -- highlight matching brackets 
+vim.opt.showmode = true         -- show vim mode in status line (default true)
+vim.opt.signcolumn = 'yes'      -- always show the sign column
+vim.opt.splitbelow = true       -- set preview window to appear at bottom
+vim.opt.splitright = true       -- set preview window to appear on right
+vim.opt.syntax = 'on'           -- enable syntax highlighting
+vim.opt.termguicolors = true    -- colorscheme support
 
 -- File locations
-vim.opt.backupdir = vim.fn.stdpath('config') .. '/backupdir'	-- location for backup files
-vim.opt.directory = vim.fn.stdpath('config') .. '/swapdir'	-- location for swap files
-vim.opt.undodir = vim.fn.stdpath('config') .. '/undodir'	-- location for undo files
-vim.opt.undofile = true			-- automatically save undo history to an undofile
+vim.opt.backupdir = vim.fn.stdpath('config') .. '/backupdir'  -- location for backup files
+vim.opt.directory = vim.fn.stdpath('config') .. '/swapdir'    -- location for swap files
+vim.opt.undodir = vim.fn.stdpath('config') .. '/undodir'      -- location for undo files
+vim.opt.undofile = true     -- automatically save undo history to an undofile
 
 -- Search
-vim.opt.hlsearch = false		-- highlight results of previous search
-vim.opt.ignorecase = true		-- ignore uppercase letters when executing search
-vim.opt.incsearch = true		-- highlight pattern matches while typing search
-vim.opt.smartcase = true		-- make search ignore uppercase letters unless the search term has uppercase
+vim.opt.hlsearch = false    -- highlight results of previous search
+vim.opt.ignorecase = true   -- ignore uppercase letters when executing search
+vim.opt.incsearch = true    -- highlight pattern matches while typing search
+vim.opt.smartcase = true    -- make search ignore uppercase letters unless the search term has uppercase
 
 -- Whitespace and indentation
-vim.opt.autoindent = true		-- copy indent from current line when starting a new line
-vim.opt.breakindent = true		-- preserve indentation of virtual lines
-vim.opt.expandtab = true		-- converts tabs to white space
-vim.opt.list = true			-- show whitespace characters
-vim.opt.listchars = 'space:·,tab:>→'	-- characters to use for whitespace
-vim.opt.shiftwidth = 2			-- width for autoindent
-vim.opt.smartindent = true		-- TODO: add description
-vim.opt.softtabstop = 2			-- TODO: add description
-vim.opt.tabstop = 2			-- number of columns occupied by a tab
-vim.opt.wrap = true			-- wrap long lines
+vim.opt.autoindent = true             -- copy indent from current line when starting a new line
+vim.opt.breakindent = true            -- preserve indentation of virtual lines
+vim.opt.expandtab = true              -- converts tabs to white space
+vim.opt.list = true                   -- show whitespace characters
+vim.opt.listchars = 'space:·,tab:-→'  -- characters to use for whitespace
+vim.opt.shiftwidth = 2                -- width for autoindent
+vim.opt.smartindent = true            -- TODO: add description
+vim.opt.softtabstop = 2               -- TODO: add description
+vim.opt.tabstop = 2                   -- number of columns occupied by a tab
+vim.opt.wrap = true                   -- wrap long lines
 
 -----------------------------------------------------------
 -- Key mappings
