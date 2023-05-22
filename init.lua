fad4ef1b9f7c231845e11dc7123833548cc2aca7
Charlie Stanard <charlie.stanard@guildeducation.com>
2023-05-22 10:06:24 -0500

    Add folke/paint to fix markdown heading colors

--- init.lua
+++ init.lua
@@ -77,6 +77,32 @@ require('lazy').setup({
     dependencies = 'kevinhwang91/promise-async',
   },
   {
+    'folke/paint.nvim',
+    config = function()
+      local hlmap = {
+        ["^#%s+(.-)%s*$"] = "Operator",
+        ["^##%s+(.-)%s*$"] = "Type",
+        ["^###%s+(.-)%s*$"] = "String",
+        ["^####%s+(.-)%s*$"] = "Constant",
+        ["^#####%s+(.-)%s*$"] = "Number",
+        ["^######%s+(.-)%s*$"] = "Error",
+      }
+
+      local highlights = {}
+      for pattern, hl in pairs(hlmap) do
+        table.insert(highlights, {
+          filter = { filetype = "markdown" },
+          pattern = pattern,
+          hl = hl,
+        })
+      end
+
+      require("paint").setup({
+        highlights = highlights,
+      })
+    end,
+  },
+  {
     'folke/twilight.nvim',
     config = function()
       require('twilight').setup()
